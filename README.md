fdfa0ff0d2f41a4e153a6c6537c9b61879ad2562
unknown <patrickviegas4@gmail.com>
2022-02-13 09:39:15 -0300

    UP API-HOTEL-FLASK :rocket:

---
+++ README.md
@@ -0,0 +1,30 @@
+# 💿 API Simples de lista de Hoteis (Primeiro projeto do curso da udemy "REST APIs com Python e Flask")
+
+
+## 📚 Sobre o projeto
+
+<p> É um sistema de API simples para listar hoteis. Esse sistema é do primeiro projeto de aula do curso da udemy sobre api com Flask.</p>
+
+<hr/>
+
+## 💻 Tecnologias
+
+<p>O sistema foi construído na linguagem de programação python e utiliza a framework Flask. Os dados estão sendo armazenados localmente, sem conexão com banco de dados. Para testar os recursos do CRUD, estou utilizando o Insomnia para enviar e receber as requisições. Por fim, é utilizado o virtualenv como ferramenta de ambiente virtual, para não misturar as bibliotecas do código original do python.</p>
+
+<hr />
+
+## ⚡ Funcionamento
+
+<p>Como ja dito, os dados são locais. Além disso, está implementado o get, post, put e delete, ou seja, o CRUD. Para executar o projeto, basta ter instalado a biblioteca do flask e do flask restful. Após isso, só executar o comando "python app.py".</p>
+
+
+<hr/>
+
+
+## 🔗 Links úteis
+
+<a href="https://flask.palletsprojects.com/en/2.0.x/">https://flask.palletsprojects.com/en/2.0.x/<a/>
+
+<a href="https://flask-restful.readthedocs.io/en/latest/">https://flask-restful.readthedocs.io/en/latest/<a/>
+
+<a href="https://www.udemy.com/course/rest-apis-com-python-e-flask/">https://www.udemy.com/course/rest-apis-com-python-e-flask/<a/>
---
+++ resources/hotel.py
@@ -0,0 +1,74 @@
+from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
+
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'Alpha Hotel',
+        'estrelas': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Rio de Janeiro'
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel',
+        'estrelas': 4.4,
+        'diaria': 380.90,
+        'cidade': 'Santa Catarina'
+    },
+    {
+        'hotel_id': 'charlie',
+        'nome': 'Charlie Hotel',
+        'estrelas': 3.9,
+        'diaria': 320.20,
+        'cidade': 'Santa Catarina'
+    }
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis}
+
+class Hotel(Resource):
+
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return None
+
+    def get(self, hotel_id):
+        print('teste')
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {'message': 'Hotel not found.'}, 404
+    
+    def post(self, hotel_id):
+        dados = Hotel.argumentos.parse_args()
+        hotel_objeto = HotelModel(hotel_id,**dados)
+        novo_hotel = hotel_objeto.json()
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
+
+    def put(self, hotel_id):   
+        dados = Hotel.argumentos.parse_args()
+        hotel_objeto = HotelModel(hotel_id,**dados)
+        novo_hotel = hotel_objeto.json()
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
+
+    def delete(self, hotel_id):
+        global hosteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message' : 'Hotel deleted'}
